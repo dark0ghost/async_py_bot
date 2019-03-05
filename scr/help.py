8bb8a41d04edd8abc868a8f3613a371a499d8fba
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-03-05 20:34:54 +0300

    Update help.py

--- scr/help.py
+++ scr/help.py
@@ -1,3 +1,23 @@
 import os
+from aiogram.utils.helper import Helper, HelperMode, ListItem
 
-package = ["aiodns","aiohttp","async-timeout","beautifulsoup4","aiogram"," aiosqlite"]
+package = ["aiodns", "aiohttp", "async-timeout", "beautifulsoup4", "aiogram", "aiosqlite", "aiosocks", "aiosocksy",
+           "aiohttp_socks"]
+
+token = " "
+
+
+mes = {
+    "start": "message start",
+    "help":"message help",
+
+}
+
+class st(Helper):
+    mode = HelperMode.snake_case
+    TEST_STATE_0 = ListItem()
+    TEST_STATE_1 = ListItem()
+    TEST_STATE_2 = ListItem()
+    TEST_STATE_3 = ListItem()
+    TEST_STATE_4 = ListItem()
+    TEST_STATE_5 = ListItem()
