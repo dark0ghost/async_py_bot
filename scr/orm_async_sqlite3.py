40d3ce83ed8d653871632c1ddfb513506ce261a1
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-03-05 20:36:37 +0300

    Update orm_async_sqlite3.py

--- scr/orm_async_sqlite3.py
+++ scr/orm_async_sqlite3.py
@@ -1,65 +1,82 @@
 import aiosqlite
-import asyncio
 
 
-class sqlite():
-    def __init__(self,connect):
+class sqlite(object):
+    def __init__(self, connect):
         self.file = connect
 
-    async def insert(self,what,table):
+    async def insert(self, what,table="data_table"):
         async with aiosqlite.connect(self.file) as db:
             await db.execute(f'INSERT INTO {table} {what}')
             await db.commit()
-            await db.close()
+
 
     async def table(self,test=0):
         if test:
             async with aiosqlite.connect(self.file) as db:
                 await db.execute(f'')
                 await db.commit()
-                await db.close()
 
 
-    async def select(self,what,table):
+
+    async def select(self, what, table="data_table"):
         async with aiosqlite.connect(self.file) as db:
             await db.execute(f'SELECT FROM {table} {what}')
             await db.commit()
-            await db.close()
 
-    async def insert_state(self,id,table):
+
+    async def insert(self, data, table="data_table"):
         async with aiosqlite.connect(self.file) as db:
-            await db.execute(f'INSERT INTO {table} {id}')
+            await db.execute(f'INSERT INTO {table} {data}')
             await db.commit()
-            await db.close()
 
-    async def create_teblae(self,create = True):
-      if create:
-        async with aiosqlite.connect(self.file) as db:
 
-            await db.execute(f'')
+    async def create_teblae(self, create: bool = True):
+       if create:
+          async with aiosqlite.connect(self.file) as db:
+
+            await db.execute(f"""
+              CREATE TABLE IF NOT EXISTS data_table (
+              id INTEGER PRIMARY KEY AUTOINCREMENT
+              );
+            """)
             await db.commit()
-            await db.close()
+            self.table = "data_table"
 
 
 
-class State(sqlite):
-    def __init__(self,table):
+
+
+class State(sqlite, object):
+    def __init__(self, table,db):
         self.table = table
-    async def crt(self,tr=1):
+        self.file = db.file
+
+
+
+    async def crt(self, tr=1):
         if tr:
             async with aiosqlite.connect(self.file) as db:
-                await db.execute(f'')
+                await db.execute(f"""
+                              CREATE TABLE IF NOT EXISTS state (
+                              id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              state STRING
+                              );
+                            """)
                 await db.commit()
-                await db.close()
 
-    async def set(self,id,state):
+                self.table = "state"
+
+    async def set(self, id, state):
         async with aiosqlite.connect(self.file) as db:
             await db.execute(f'INSERT INTO {self.table} {id} {state}')
             await db.commit()
-            await db.close()
+
 
     async def deleted(self):
         async with aiosqlite.connect(self.file) as db:
             await db.execute(f'INSERT INTO {self.table} ')
             await db.commit()
-            await db.close()
+
+
+
