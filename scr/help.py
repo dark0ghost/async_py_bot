d39b4884ce874a6307f5cd8522e9e57ed39fbc5e
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-06-23 22:26:22 +0300

    Update help.py

--- scr/help.py
+++ scr/help.py
@@ -2,7 +2,7 @@ package: list = ["aiodns", "aiohttp", "async-timeout", "beautifulsoup4", "aiogra
                  "aiosocksy",
                  "aiohttp_socks"]
 
-token: str = " "
+token: str = ""
 
 mes: dict = {
     "start": "message start",
@@ -12,4 +12,13 @@ mes: dict = {
 import aiohttp
 good_proxy_link: str = "socks5://exp1.s5overss.mtpro.xyz:39610"
 login = aiohttp.BasicAuth(login='mtpro_xyz', password='mtpro_xyz_bot')
+
+from aiogram.utils.helper import Helper, HelperMode, ListItem
+
+
+class state(Helper):
+    mode = HelperMode.snake_case
+    END = ListItem()
+    START = ListItem()
+    pass
 
