a94ae5de41d60179c626e7f13295911251f53dd0
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-06-24 20:57:24 +0300

    Update help.py

--- scr/help.py
+++ scr/help.py
@@ -1,24 +1,34 @@
+import aiohttp
+
+from aiogram import Bot
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
 package: list = ["aiodns", "aiohttp", "async-timeout", "beautifulsoup4", "aiogram", "aiosqlite", "aiosocks",
                  "aiosocksy",
                  "aiohttp_socks"]
 
-token: str = ""
+token: str = " "
 
 mes: dict = {
     "start": "message start",
     "help": "message help",
 
 }
-import aiohttp
+
 good_proxy_link: str = "socks5://exp1.s5overss.mtpro.xyz:39610"
 login = aiohttp.BasicAuth(login='mtpro_xyz', password='mtpro_xyz_bot')
 
-from aiogram.utils.helper import Helper, HelperMode, ListItem
+
+async def get_link(bot: Bot):
+    link_bot = (await bot.get_me()).username
+
+    link: str = f"t.me/{link_bot}/?ref="
+    return link
 
 
-class state(Helper):
-    mode = HelperMode.snake_case
-    END = ListItem()
-    START = ListItem()
+class state(StatesGroup):
+    start: State = State()
+    end: State = State()
     pass
+
 
