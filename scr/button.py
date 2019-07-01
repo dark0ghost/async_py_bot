c52b051168816b0fa2b93d07713c4326c65fa650
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-07-01 22:07:16 +0300

    Update button.py

--- scr/button.py
+++ scr/button.py
@@ -1,43 +1,48 @@
 # This Python file uses the following encoding: utf-8
 
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup,InlineKeyboardButton
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
+    InlineKeyboardButton
 
 
 class button:
-    @staticmethod
-    def start() -> InlineKeyboardMarkup:
-        inline_btn_1 = InlineKeyboardButton('Первая кнопка', callback_data='button')
+    def button_all(text_button: str, callback: str) -> InlineKeyboardMarkup:
+        inline_btn_1 = InlineKeyboardButton(text=text_button, callback_data=callback)
         inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
         return inline_kb1
-    @staticmethod
-    def proxy(list_proxy:list) -> InlineKeyboardMarkup:
+
+    def proxy(list_proxy: list) -> InlineKeyboardMarkup:
         inline_kb1 = InlineKeyboardMarkup()
         for i in list_proxy:
-           s:str= i.replace("socks5://", "")
-           server,port =s.split(":")
-           link_beta: str = f"https://t.me/proxy?server={server}&port={port}"
-           inline_kb1.add(InlineKeyboardButton(server, url=link_beta))
-        inline_btn_1 = InlineKeyboardButton('Первая кнопка',callback_data='button')
-        inline_kb1.add(inline_btn_1)
+            s: str = i.replace("socks5://", "")
+            server, port = s.split(":")
+            link_beta: str = f"https://t.me/proxy?server={server}&port={port}"
+            inline_kb1.add(InlineKeyboardButton(server, url=link_beta))
         return inline_kb1
-
 
 
 class keyboard:
-    #resize_keyboard=True, one_time_keyboard=True
-    @staticmethod
-    def start(*args:str) -> ReplyKeyboardMarkup:
-        button_hi = KeyboardButton(args)
+    # resize_keyboard=True, one_time_keyboard=True
+    def keyboard_all(text: str) -> ReplyKeyboardMarkup:
+        button_hi = KeyboardButton(text=text)
         greet_kb = ReplyKeyboardMarkup(resize_keyboard=True)
         greet_kb.add(button_hi)
         return greet_kb
 
     @staticmethod
-    def remove() -> ReplyKeyboardRemove:
+    def remove_kaeyboard() -> ReplyKeyboardRemove:
+        """
+
+        
+        :param kwargs: 
+        :return: 
+        """
         return ReplyKeyboardRemove()
 
-    @staticmethod
-    def get_contact()-> ReplyKeyboardMarkup:
-        return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Отправить свой контакт ', request_contact=True))
+    def get_contact(text_contact: str) -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton(text=text_contact, request_contact=True))
+
+    def geotag(text_geo: str) -> ReplyKeyboardMarkup:
+        
+        return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton(text=text_geo, request_location=True))
 
 
