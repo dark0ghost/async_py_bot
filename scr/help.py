344b80d61a9308e0c5868c4a4cb683e09b865e92
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-07-02 21:49:18 +0300

    Update help.py

--- scr/help.py
+++ scr/help.py
@@ -31,3 +31,25 @@ class state(StatesGroup):
     conact: State = State()
     geo:State = State()
     pass
+  
+  
+  class e_mail:
+    def __init__(self, mail: str):
+        self.mail = mail
+
+    def is_e_mail(self):
+        return True if (("@" in self.mail) and ("." in self.mail)) else False
+
+    def is_gmail(self):
+        return True if ("gamil.com" in (self.mail.split("@"))[1]) else False
+
+    def is_mail(self):
+        return True if ("mail.ru" in (self.mail.split("@"))[1]) else False
+
+    def is_yandex(self):
+        return True if ("yandex.ru" in (self.mail.split("@"))[1]) else False
+
+    def is_yahoo(self):
+        return True if ("yahoo.com" in (self.mail.split("@"))[1]) else False
+    def get_mail(self):
+        return self.mail
