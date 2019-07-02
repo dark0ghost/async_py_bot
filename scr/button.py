87f6ea3f81ae5c5d5748ee27b322534697ab6440
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-07-02 22:09:56 +0300

    Update button.py

--- scr/button.py
+++ scr/button.py
@@ -6,18 +6,25 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
 
 class button:
     def button_all(text_button: str, callback: str) -> InlineKeyboardMarkup:
-        inline_btn_1 = InlineKeyboardButton(text=text_button, callback_data=callback)
-        inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
-        return inline_kb1
+        inline_btn = InlineKeyboardButton(text=text_button, callback_data=callback)
+        inline_kb = InlineKeyboardMarkup().add(inline_btn)
+        return inline_kb
 
     def proxy(list_proxy: list) -> InlineKeyboardMarkup:
-        inline_kb1 = InlineKeyboardMarkup()
+        inline_kb = InlineKeyboardMarkup()
         for i in list_proxy:
             s: str = i.replace("socks5://", "")
             server, port = s.split(":")
             link_beta: str = f"https://t.me/proxy?server={server}&port={port}"
-            inline_kb1.add(InlineKeyboardButton(server, url=link_beta))
-        return inline_kb1
+            inline_kb.add(InlineKeyboardButton(server, url=link_beta))
+        return inline_kb
+
+    def butoons(text: list, call_back: list)-> InlineKeyboardMarkup :
+        inline_kb: InlineKeyboardMarkup = InlineKeyboardMarkup()
+        for val, i in enumerate(text):
+            inb = InlineKeyboardButton(text=i, callback_data=call_back[i])
+            inline_kb.add(inb)
+        return inline_kb
 
 
 class keyboard:
