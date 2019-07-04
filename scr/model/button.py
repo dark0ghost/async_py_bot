ab2a8edd67bb523dc905a4f19511025814ab64d4
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-07-04 13:16:24 +0300

    Update button.py

--- scr/model/button.py
+++ scr/model/button.py
@@ -1,14 +1,21 @@
 # This Python file uses the following encoding: utf-8
 
-from aiogram.types import InlineKeyboardMarkup,  InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
+from typing import List
 
-class button:
-    def button_all(text_button: str, callback: str) -> InlineKeyboardMarkup:
-        inline_btn = InlineKeyboardButton(text=text_button, callback_data=callback)
+
+class Button:
+    posts_cb: CallbackData = CallbackData('post', 'id', 'action')
+
+    @classmethod
+    def button_all(cls, text_button: str, callback: str) -> InlineKeyboardMarkup:
+        inline_btn = InlineKeyboardButton(text=text_button,
+                                          callback_data=cls.posts_cb.new(id=callback, action=text_button))
         inline_kb = InlineKeyboardMarkup().add(inline_btn)
         return inline_kb
 
-    def proxy(list_proxy: list) -> InlineKeyboardMarkup:
+    def proxy(list_proxy: List[str]) -> InlineKeyboardMarkup:
         inline_kb = InlineKeyboardMarkup()
         for i in list_proxy:
             s: str = i.replace("socks5://", "")
@@ -17,9 +24,22 @@ class button:
             inline_kb.add(InlineKeyboardButton(server, url=link_beta))
         return inline_kb
 
-    def butoons(text: list, call_back: list)-> InlineKeyboardMarkup:
+    @classmethod
+    def butoons(cls, text: List[str], call_back: list) -> InlineKeyboardMarkup:
         inline_kb: InlineKeyboardMarkup = InlineKeyboardMarkup()
         for val, i in enumerate(text):
-            inb = InlineKeyboardButton(text=i, callback_data=call_back[i])
+            inb = InlineKeyboardButton(text=i, callback_data=cls.posts_cb.new(id=call_back[i]))
             inline_kb.add(inb)
+        return inline_kb
+
+    @classmethod
+    def edit_proxy(cls, text_button: str, proxy: str, callback: str = "proxy") -> InlineKeyboardMarkup:
+        proxy = proxy.replace("socks5://", "")
+        server, port = proxy.split(":")
+        inline_btn = InlineKeyboardButton(text=text_button,
+                                          callback_data=Button.posts_cb.new(id=callback, action=text_button))
+        link_beta: str = f"https://t.me/proxy?server={server}&port={port}"
+        inline_kb = InlineKeyboardMarkup()
+        inline_kb.add(InlineKeyboardButton(text="proxy for you", url=link_beta))
+        inline_kb.add(inline_btn)
         return inline_kb
