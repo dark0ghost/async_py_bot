ea533eebd0296584122d3a5209120e068f3d2acb
dark0ghost <diamondlego018@gmail.com>
2019-10-08 23:42:21 +0300

    more.....

--- modules/org/ton/Ton.py
+++ modules/org/ton/Ton.py
@@ -26,20 +26,16 @@ class Ton:
             print(response)
 
     async def getaccountforms(self, address: str):
-        base: str = base64.urlsafe_b64encode(address.encode(encoding="UTF-8"))
-        base = str(base).replace("b'", "").replace("'","")
         async with self.session.post(url=self.ton_test,
                                      json={"jsonrpc": "2.0", "id": 1, "method": "getaccountforms",
-                                           "params": ["-1:8156775b79325e5d62e742d9b96c30b6515a5cd2f1f64c5da4b193c03f070e0d"]}, headers={'Content-Type': 'application/json'}) as response:
-            print({"jsonrpc": "2.0", "id": 1, "method": "getaccountforms",
-                   "params": [f"-1:{base}"]})
-            print(await response.json())
+                                           "params": [address]}, headers={'Content-Type': 'application/json'}) as response:
+            return await response.json()
 
 
 async def n():
     async with aiohttp.ClientSession() as session:
         d = Ton(session=session)
-        await d.getaccountforms(address="kQD8uRo6OBbQ97jCx2EIuKm8Wmt6Vb15-KsQHFLbKSMiYHa6")
+        print( await d.getaccountforms(address="kQD8uRo6OBbQ97jCx2EIuKm8Wmt6Vb15-KsQHFLbKSMiYHa6"))
 
 
 asyncio.run(n())
