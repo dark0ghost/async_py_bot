66c3b135c8964886e14318f919b581b61d98a4aa
dark0ghost <diamondlego018@gmail.com>
2019-10-10 23:39:49 +0300

    more.....

--- modules/modificators.py
+++ modules/modificators.py
@@ -2,12 +2,12 @@ from typing import Callable
 
 
 class override:
-    def __call__(self, func: Callable):
+    def __call__(self, func: Callable) -> Callable:
         func.__doc__ = "this override  method"
         return func
 
 
 class private:
-    def __call__(self, func: Callable, *args, **kwargs):
+    def __call__(self, func: Callable, *args, **kwargs) -> Callable:
         func.__doc__ = "this private  method"
         return func
