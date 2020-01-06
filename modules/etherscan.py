314c68ec8bbe5d6216598999e53cb0faffcb8449
dark0ghost <diamondlego018@gmail.com>
2020-01-06 23:43:27 +0300

    make etherscan class api

--- modules/etherscan.py
+++ modules/etherscan.py
@@ -6,13 +6,25 @@ from aiohttp_socks import SocksConnector
 
 
 class Etherscan:
+    """
+    @see
+    DOC FOR USE :
+     async def main():
+       f = Etherscan(api_key="{token}")
+        await f.open_session() # or Etherscan(api_key="{token}",session)
+        print(await f.your_method())
+     asyncio.run(main())
+    """
 
     class BadRequest(Exception):
+        """
+        @doc
+        class for custom exception
+        """
         pass
 
     def __init__(self, api_key: str, session: typing.Optional[aiohttp.ClientSession] = None) -> None:
         """
-
         @param api_key:
         @param session:
         """
@@ -21,19 +33,18 @@ class Etherscan:
         self.api: str = api_key
 
     def __str__(self) -> str:
+        """
+
+        @return:
+        """
         return f"{self.__class__.__name__}({self.api})"
 
     def __hash__(self) -> int:
+        """
+
+        @return:
+        """
         return hash(self.api + self.__str__())
-
-    def is_limit_time(self) -> bool:
-        """
-
-        @rtype: object
-        """
-        then = datetime.datetime.now()
-        delta = self.now - then
-        return delta.seconds > 1
 
     async def open_session(self, proxy: typing.Optional[str] = None) -> aiohttp.ClientSession:
         """
@@ -49,7 +60,7 @@ class Etherscan:
 
     async def close(self) -> None:
         """
-
+        close session
         @return:
         """
         await self.session.close()
@@ -188,8 +199,8 @@ class Etherscan:
                 f"https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey={self.api}") as response:
             return await response.json()
 
-    async def eth_estimateGas(self, to: str, gasprice: str, gas: int, valvue: int = 0xff22) -> typing.Dict[
-        str, typing.Any]:
+    async def eth_estimateGas(self, to: str, gasprice: str, gas: int, valvue: int = 0xff22) -> \
+            typing.Dict[str, typing.Any]:
         """
         Makes a call or transaction, which won't be added to the blockchain and returns the used gas, which can be used for estimating the used gas
         @param gas:
@@ -292,8 +303,141 @@ class Etherscan:
                 f"https://api.etherscan.io/api?module=account&action=txlistinternal&txhash={__hash}&apikey={self.api}") as response:
             return await response.json()
 
-    async def list_of_ERC20_token_transfer_events__by_address(self) -> typing.Dict[str, typing.Any]:
-        pass
+    async def contract_abi_for_verified_contract_source_codes(self, address: str) -> typing.Dict[str, typing.Any]:
+        """
+        Get Contract ABI for Verified Contract Source Codes
+        @param address:
+        @return:
+        """
+        async with self.session.get(
+                f"https://api.etherscan.io/api?module=contract&action=getabi&address={address}&apikey={self.api}") as response:
+            return await response.json()
+
+    async def get_source_code_contract(self, address: str) -> typing.Dict[str, typing.Any]:
+        """
+
+        @param address:
+        @return:
+        """
+        async with self.session.get(
+                f"https://api.etherscan.io/api?module=contract&action=getsourcecode&address={address}&apikey={self.api}") as response:
+            return await response.json()
+
+    async def check_source_code_verification_submission_status(self, guid: str, module: str = "contract",
+                                                               action: str = "checkverifystatus") \
+            -> typing.Dict[str, typing.Any]:
+        """
+
+        @param guid:
+        @param module:
+        @param action:
+        @return:
+        """
+        data: typing.Dict[str, str] = {
+            "guid": guid,
+            "module": module,
+            "action": action
+        }
+        async with self.session.get(url="https://api.etherscan.io/api", data=data) as response:
+            return await response.json()
+
+    async def check_contract_execution_status(self, txhash: str) -> typing.Dict[str, typing.Any]:
+        """
+        (if there was an error during contract execution)
+        Note: isError":"0" = Pass , isError":"1" = Error during Contract Execution
+        @param txhash:
+        @return:
+        """
+        async with self.session.get(
+                f"https://api.etherscan.io/api?module=transaction&action=getstatus&txhash={txhash}&apikey={self.api}") as response:
+            return await response.json()
+
+    async def block_and_uncle_rewards_by_blockno(self, blockno: typing.Union[int, str]) -> typing.Dict[str, typing.Any]:
+        """
+        Get Block And Uncle Rewards by BlockNo
+        @param blockno:
+        @return:
+        """
+        async with self.session.get(
+                f"https://api.etherscan.io/api?module=block&action=getblockreward&blockno={blockno}&apikey={self.api}") as response:
+            return await response.json()
+
+    async def check_transaction_receipt_status(self, txhash: str) -> typing.Dict[str, typing.Any]:
+        """
+        (Only applicable for Post Byzantium fork transactions)
+        Note: status: 0 = Fail, 1 = Pass. Will return null/empty value for pre-byzantium fork
+        @param txhash:
+        @return:
+        """
+        async with self.session.get(
+                f"https://api.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash={txhash}&apikey={self.api}") as response:
+            return await response.json()
+
+    async def estimated_block_countdown_time_by_blockno(self, blockno: typing.Union[int, str]) -> typing.Dict[
+        str, typing.Any]:
+        """
+        Get Estimated Block Countdown Time by BlockNo
+        @param blockno:
+        @return:
+        """
+        async with self.session.get(
+                f"https://api.etherscan.io/api?module=block&action=getblockcountdown&blockno={blockno}&apikey={self.api}") as response:
+            return await response.json()
+
+    async def list_of_ERC20_token_transfer_events__by_address(self, address: str, start_block: int = 0,
+                                                              end_block: int = 999999999,
+                                                              offset: typing.Optional[typing.Union[str, int]] = None,
+                                                              page: typing.Optional[typing.Union[int, str]] = None) -> \
+            typing.Dict[
+                str, typing.Any]:
+        """
+        [Optional Parameters] startblock: starting blockNo to retrieve results, endblock: ending blockNo to retrieve results
+        http://api.etherscan.io/api?module=account&action=tokentx&address=0x4e83362442b8d1bec281594cea3050c8eb01311c&startblock=0&endblock=999999999&sort=asc&apikey=YourApiKeyToken
+        (Returns up to a maximum of the last 10000 transactions only)
+        or
+        https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2&page=1&offset=100&sort=asc&apikey=YourApiKeyToken
+        (To get paginated results use page=<page number> and offset=<max records to return>)
+        or
+        https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2&address=0x4e83362442b8d1bec281594cea3050c8eb01311c&page=1&offset=100&sort=asc&apikey=YourApiKeyToken
+        (To get transfer events for a specific token contract, include the contractaddress paramter)
+        @param address:
+        @param start_block:
+        @param end_block:
+        @param offset:
+        @param page:
+        @return:
+        """
+        link = f"http://api.etherscan.io/api?module=account&action=tokentx&address={address}&startblock={start_block}&endblock={end_block}&sort=asc&apikey={self.api} "
+        if not (offset is None):
+            link += f"&offset={offset}"
+        if not (page is None):
+            link += f"&page={page}"
+        async with self.session.get(link) as response:
+            return await response.json()
+
+    async def list_of_blocks_mined_by_address(self, address: str,
+                                              offset: typing.Optional[typing.Union[str, int]] = None,
+                                              page: typing.Optional[typing.Union[int, str]] = None) -> \
+            typing.Dict[
+                str, typing.Any]:
+        """
+        https://api.etherscan.io/api?module=account&action=getminedblocks&address=0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b&blocktype=blocks&apikey=YourApiKeyToken
+        or
+        https://api.etherscan.io/api?module=account&action=getminedblocks&address=0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b&blocktype=blocks&page=1&offset=10&apikey=YourApiKeyToken
+        (To get paginated results use page=<page number> and offset=<max records to return>)
+        ** type = blocks (full blocks only) or uncles (uncle blocks only)
+        @param address:
+        @param offset:
+        @param page:
+        @return:
+        """
+        link: str = f"https://api.etherscan.io/api?module=account&action=getminedblocks&address={address}&blocktype=blocks&apikey={self.api}"
+        if not (offset is None):
+            link += f"&offset={offset}"
+        if not (page is None):
+            link += f"page={page}"
+        async with self.session.get(link) as response:
+            return await response.json()
 
 
 import asyncio
@@ -302,36 +446,7 @@ import asyncio
 async def main():
     f = Etherscan(api_key="")
     await f.open_session()
-
-    print(await f.eth_blockNumber())
-    print(await f.eth_getBlockByNumber("0x9d4f"))
-    print(await f.eth_getTransactionByBlockNumberAndIndex("0x9d4f"))
-    print(await f.eth_getBlockTransactionCountByNumber("0x9d4f"))
-    print(await f.eth_getTransactionByHash("0x1e2910a262b1008d0616a0beb24c1a491d78771baa54a33e66065e03b1f46bc1"))
-    print(await f.eth_getUncleByBlockNumberAndIndex("0x10d4f"))
-    print(await f.eth_getTransactionCount("0x2910543af39aba0cd09dbb2d50200b3e800a63d2"))
-    print(await f.eth_sendRawTransaction("0xf904808000831cfde080"))
-    print(await f.eth_getTransactionReceipt("0x1e2910a262b1008d0616a0beb24c1a491d78771baa54a33e66065e03b1f46bc1"))
-    print(await f.eth_call(to="0xAEEF46DB4855E25702F8237E8f403FddcaF931C0",
-                           data="0x70a08231000000000000000000000000e16359506c028e51f16be38986ec5746251e9724"))
-    print(await f.eth_getBlockByNumber("0xf75e354c5edc8efed9b59ee9f67a80845ade7d0c"))
-    print(await f.eth_getStorageAt("0x6e03d9cce9d60f3e9f2597e13cd4c54c55d3e433"))
-    print(await f.eth_gasPrice())
-    print(
-        await f.eth_estimateGas(to="0xf0160428a8552ac9bb7e050d90eeade4ddd52843", gasprice="0x051da038cc", gas=0xffffff))
-    print(await f.ether_balance_single_address("0x2a6e46570566659dd87939e20f0857b26c966749"))
-    print(await f.ether_balance_multiple_addresses(["0xAEEF46DB4855E25702F8237E8f403FddcaF931C0",
-                                                    "0x2a6e46570566659dd87939e20f0857b26c966749"]))
-    print(await f.list_of_normal_transactions("0x2A6E45970566659DD87939E20f0857B26c966749"))
-    print(await f.list_of_normal_transactions("0x2A6E45970566659DD87939E20f0857B26c966749"))
-    print(await f.list_of_normal_transactions("0x2A6E45970566659DD87939E20f0857B26c966749", 10))
-    print(await f.list_of_internal_transactions_by_address("0x2A6E45970566659DD87939E20f0857B26c966749"))
-    print(await f.list_of_internal_transactions_by_address("0x2A6E45970566659DD87939E20f0857B26c966749", 10))
-    print(await f.internal_transactions_by_transaction_hash(
-        "0x40eb908387324f2b575b4879cd9d7188f69c8fc9d87c901b9e2daaea4b442170"))
-    print(hash(f))
-    print(f)
-
+    print(await f.list_of_blocks_mined_by_address("0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b",page=1,offset=1))
     await f.close()
 
 
